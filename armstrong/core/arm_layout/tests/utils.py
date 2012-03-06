16f81abfcfedfcb329f41d06942c2cd74bbedd2f
Jon Cotton <jcotton@baycitizen.org>
2012-03-06 11:22:23 -0800

    Improve testing function names. Separate out components into their own tests. Remove an erroneous assertTrue().

--- armstrong/core/arm_layout/tests/utils.py
+++ armstrong/core/arm_layout/tests/utils.py
@@ -20,18 +20,19 @@ class get_layout_template_nameTestCase(ArmstrongTestCase):
         self.m._meta.app_label = self._original_app_label
         super(get_layout_template_nameTestCase, self).tearDown()
 
-    def test_get_layout_template_name(self):
-        """Test that the proper template path is returned"""
+    def test_requires_a_model_instance(self):
+        with self.assertRaises(TypeError):
+            get_layout_template_name(type(self.m), self.name)
 
+    def test_returns_proper_path(self):
         result = get_layout_template_name(self.m, self.name)
         expected = 'layout/%s/%s/%s.html' % \
             (self.m._meta.app_label,
              self.m._meta.object_name.lower(),
              self.name)
-        self.assertTrue(isinstance(result, list))  # assertIsInstance() new in Python 2.7
         self.assertEqual([expected], result)
 
-        # okay if there isn't an actual file
+    def test_missing_file_is_okay(self):
         file_doesnt_exist = "fake_template"
         result = get_layout_template_name(self.m, file_doesnt_exist)
         expected = 'layout/%s/%s/%s.html' % \
@@ -40,37 +41,27 @@ class get_layout_template_nameTestCase(ArmstrongTestCase):
              file_doesnt_exist)
         self.assertEqual([expected], result)
 
-        # requires an instance
-        with self.assertRaises(TypeError):
-            get_layout_template_name(type(self.m), self.name)
-
-    def test_get_layout_template_random_app_name(self):
-        """Test that the proper template path is returned"""
-
+    def test_uses_app_label_in_template_name(self):
         self.m._meta.app_label = "random_%d" % random.randint(100, 200)
         result = get_layout_template_name(self.m, self.name)
         expected = 'layout/%s/foobar/%s.html' % \
             (self.m._meta.app_label, self.name)
         self.assertEqual([expected], result)
 
-    def test_get_layout_template_random_model_name(self):
-        """Test that the proper template path is returned"""
-
+    def test_uses_model_name_in_template_name(self):
         self.m._meta.object_name = "random_%d" % random.randint(100, 200)
         result = get_layout_template_name(self.m, self.name)
         expected = 'layout/arm_layout_support/%s/%s.html' % \
             (self.m._meta.object_name, self.name)
         self.assertEqual([expected], result)
 
-    def test_get_layout_template_random_tpl_name(self):
-        """Test that the proper template path is returned"""
-
+    def test_uses_name_in_template_name(self):
         name = "random_%d" % random.randint(100, 200)
         result = get_layout_template_name(self.m, name)
         expected = 'layout/arm_layout_support/foobar/%s.html' % name
         self.assertEqual([expected], result)
 
-    def test_get_layout_template_inheritance(self):
+    def test_can_find_templates_through_models_inheritance(self):
         """
         Test that the proper order of templates in the
         Model inheritance structure is returned
